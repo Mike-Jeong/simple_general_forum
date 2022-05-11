7b04a8a12f4610eb4f17f2591a52c0f600faad28
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-12 05:56:38 +0900

    error 메시지를 바탕으로 코드 수정_ver.01

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import React from "react";
+
 import {
+  Route,
+  Routes,
+  NavLink,
   BrowserRouter as Router,
 } from "react-router-dom";
+
 import './App.css';
 
 import Home from "./pages/Home";
 import ArticleList from "./pages/ArticleList";
-import SubmitArticle from "./pages/Submit-Article"; 
+import SubmitArticle from "./pages/Submit-Article";
 import NotFoundPage from "./pages/404";
 
-function App() {
-    return ( 
-        <Router>
-        <div>
-          <h1>민식이의 익명 게시판 프로젝트(프로젝트 명 미정...)</h1>
-            <ul className="header">
-                <li><a href = "/">메인 화면</a></li>
-                <li><a href = "/ArticleList">게시판 목록</a></li>
-                <li><a href = "/SubmitArticle">게시판 등록</a></li>
-            </ul>
-          <div className="content">
-            
-          </div>
-        </div>
-        </Router>
-    );
-}
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <h1>민식이의 익명 게시판 프로젝트(프로젝트 명 미정…)</h1>
+        <ul className="header">
+          <li><NavLink exact="true" to="/">메인 화면</NavLink></li>
+          <li><NavLink to="/ArticleList">게시판 목록</NavLink></li>
+          <li><NavLink to="/SubmitArticle">게시판 등록</NavLink></li>
+        </ul>
 
+        <div className="content">
+          <Routes>
+            <Route exact="true" path="/" component={Home} />
+            <Route path="/ArticleList" component={ArticleList} />
+            <Route path="/SubmitArticle" component={SubmitArticle} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
