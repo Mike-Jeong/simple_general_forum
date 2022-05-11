0a99d3caec19c920e1474dd8773e48c3f7cd1324
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-12 00:05:53 +0900

    11052022 화면 프래임 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Home from "./pages/Home";
+import ArticleList from "./pages/ArticleList";
+import SubmitArticle from "./pages/Submit-Article"; 
+import NotFoundPage from "./pages/404";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        <Router>
+        <div>
+          <h1>민식이의 익명 게시판 프로젝트(프로젝트 명 미정...)</h1>
+            <ul className="header">
+                <li><a href = "/">메인 화면 입니다</a></li>
+                <li><a href = "/ArticleList">게시판 태이블이 들어갈 자리 입니다</a></li>
+                <li><a href = "/SubmitArticle">게시판 등록이 들어갈 자리 입니다</a></li>
+            </ul>
+          <div className="content">
+            
+          </div>
+        </div>
+        </Router>
+    );
 }
+
 
 export default App;
