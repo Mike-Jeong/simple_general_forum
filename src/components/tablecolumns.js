99840d8029557a849077703be97133e8d32b1856
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-14 17:31:12 +0900

    게시글 페이지 테이블 작업

---
+++ src/components/tablecolumns.js
@@ -0,0 +1,15 @@
+export const tablecolumns = [
+    {
+        Header: '게시글 id',
+        accessor: 'id'
+    }, {
+        Header: '작성자',
+        accessor: 'authors'
+    }, {
+        Header: '제목',
+        accessor: 'title',
+    }, {
+        Header: '작성일',
+        accessor: 'createdate'
+    }
+];
--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -1,12 +1,22 @@
-import React from "react";
+import articles from "../dummydata/articles.js";
+import Styles from "../components/tablestyle.js";
+import Table from "../components/posttable";
+import tablecolumns from "../components/tablecolumns.js";
+
  
 const ArticleList = () => {
-    return (
-      <div>
-        <h2>여기는 등록된 글들이 보여질 공간 입니다</h2>
-        <p>테이블 형식으로 글들이 보일겁니다</p>
-      </div>
-    );
+  return (
+    <div>
+      <h2>테이블 생성!!!</h2>
+             <Styles>
+               <Table
+                data={articles}
+                columns={tablecolumns}
+               />
+            </Styles>
+    </div>
+  );
 }
+
  
 export default ArticleList;
