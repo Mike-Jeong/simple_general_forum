b2ba14d1de8424c2983f9aa8b955883cc893826d
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-14 17:45:12 +0900

    더미데이터 입력

---
+++ src/dummydata/articles.js
@@ -0,0 +1,27 @@
+const articles = [
+    {
+        id: "1",
+        authors: "정민식",
+        title: "첫번째 글 입니다",
+        createdate: "2022.05.01"
+    },
+    {
+        id: "2",
+        authors: "민식이냐",
+        title: "두번째 글 입니다",
+        createdate: "2022.05.02"
+    },
+    {
+        id: "3",
+        authors: "감자",
+        title: "세번쩨 글 입니다",
+        createdate: "2022.05.03"
+    },
+    {
+        id: "4",
+        authors: "싹난 감자",
+        title: "네번쩨 글 입니다",
+        createdate: "2022.05.04"
+    }
+];
+module.exports = articles;
