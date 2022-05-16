588ed53af6aec700fe5297b52a285fe7be18ef05
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-16 23:24:46 +0900

    dummy data 추가

--- src/dummydata/articles.js
+++ src/dummydata/articles.js
@@ -22,6 +22,175 @@ const articles = [
         authors: "싹난 감자",
         title: "네번쩨 글 입니다",
         createdate: "2022.05.04"
-    }
+    },
+    {
+        id: "5",
+        authors: "정민식",
+        title: "5번쩨 글 입니다",
+        createdate: "2022.05.05"
+    },
+    {
+        id: "6",
+        authors: "정민식",
+        title: "6번쩨 글 입니다",
+        createdate: "2022.05.06"
+    },
+    {
+        id: "7",
+        authors: "정민식",
+        title: "7번쩨 글 입니다",
+        createdate: "2022.05.07"
+    },
+    {
+        id: "8",
+        authors: "정민식",
+        title: "8번쩨 글 입니다",
+        createdate: "2022.05.08"
+    },
+    {
+        id: "9",
+        authors: "정민식",
+        title: "9번째 글 입니다",
+        createdate: "2022.05.09"
+    },
+    {
+        id: "10",
+        authors: "정민식",
+        title: "10번쩨 글 입니다",
+        createdate: "2022.05.10"
+    },
+    {
+        id: "11",
+        authors: "정민식",
+        title: "11번쩨 글 입니다",
+        createdate: "2022.05.11"
+    },
+    {
+        id: "12",
+        authors: "싹난 감자",
+        title: "12번쩨 글 입니다",
+        createdate: "2022.05.12"
+    },
+    {
+        id: "13",
+        authors: "정민식",
+        title: "13번째 글 입니다",
+        createdate: "2022.05.13"
+    },
+    {
+        id: "14",
+        authors: "정민식",
+        title: "14번째 글 입니다",
+        createdate: "2022.05.15"
+    },
+    {
+        id: "15",
+        authors: "감자",
+        title: "15번쩨 글 입니다",
+        createdate: "2022.05.15"
+    },
+    {
+        id: "16",
+        authors: "싹난 감자",
+        title: "16번쩨 글 입니다",
+        createdate: "2022.05.16"
+    },
+    {
+        id: "17",
+        authors: "정민식",
+        title: "17번째 글 입니다",
+        createdate: "2022.05.01"
+    },
+    {
+        id: "18",
+        authors: "민식이냐",
+        title: "18번째 글 입니다",
+        createdate: "2022.05.02"
+    },
+    {
+        id: "19",
+        authors: "감자",
+        title: "19번쩨 글 입니다",
+        createdate: "2022.05.03"
+    },
+    {
+        id: "20",
+        authors: "싹난 감자",
+        title: "20번쩨 글 입니다",
+        createdate: "2022.05.04"
+    },
+    {
+        id: "21",
+        authors: "정민식",
+        title: "21번쩨 글 입니다",
+        createdate: "2022.05.05"
+    },
+    {
+        id: "22",
+        authors: "정민식",
+        title: "22번쩨 글 입니다",
+        createdate: "2022.05.06"
+    },
+    {
+        id: "23",
+        authors: "정민식",
+        title: "23번쩨 글 입니다",
+        createdate: "2022.05.07"
+    },
+    {
+        id: "24",
+        authors: "정민식",
+        title: "24번쩨 글 입니다",
+        createdate: "2022.05.08"
+    },
+    {
+        id: "25",
+        authors: "정민식",
+        title: "25번째 글 입니다",
+        createdate: "2022.05.09"
+    },
+    {
+        id: "26",
+        authors: "정민식",
+        title: "26번쩨 글 입니다",
+        createdate: "2022.05.10"
+    },
+    {
+        id: "27",
+        authors: "정민식",
+        title: "27번쩨 글 입니다",
+        createdate: "2022.05.11"
+    },
+    {
+        id: "28",
+        authors: "싹난 감자",
+        title: "28번쩨 글 입니다",
+        createdate: "2022.05.12"
+    },
+    {
+        id: "29",
+        authors: "정민식",
+        title: "29번째 글 입니다",
+        createdate: "2022.05.13"
+    },
+    {
+        id: "30",
+        authors: "정민식",
+        title: "30번째 글 입니다",
+        createdate: "2022.05.15"
+    },
+    {
+        id: "31",
+        authors: "감자",
+        title: "31번쩨 글 입니다",
+        createdate: "2022.05.15"
+    },
+    {
+        id: "32",
+        authors: "싹난 감자",
+        title: "32번쩨 글 입니다",
+        createdate: "2022.05.16"
+    },
+
 ];
 module.exports = articles;
