90866f68cbdee0e548558aca4107017a85508a79
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-16 23:01:46 +0900

    pagination 기능 구현

--- src/components/posttable.js
+++ src/components/posttable.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useTable, useSortBy } from 'react-table';
+import { useTable, useSortBy, usePagination } from 'react-table';
 
 const Table = ({ columns, data }) => {
     const {
@@ -8,13 +8,23 @@ const Table = ({ columns, data }) => {
         headerGroups,
         prepareRow,
         page,
-        rows,
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: { pageIndex, pageSize },
     } = useTable(
         {
             columns,
-            data
+            data,
+            initialState: { pageIndex: 0, pageSize: 10},
         },
-        useSortBy
+        useSortBy,
+        usePagination
     )
 
 
@@ -40,11 +50,11 @@ const Table = ({ columns, data }) => {
                     ))}
                 </thead>
                 <tbody {...getTableBodyProps()}>
-                    {rows.map((row, i) => {
-                        prepareRow(row)
+                    {page.map((page, i) => {
+                        prepareRow(page)
                         return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map(cell => {
+                            <tr {...page.getRowProps()}>
+                                {        page.cells.map(cell => {
                                     return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
                                 })}
                             </tr>
@@ -52,6 +62,50 @@ const Table = ({ columns, data }) => {
                     })}
                 </tbody>
             </table>
+            <div className="pagination">
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>{' '}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </button>{' '}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                </button>{' '}
+                <span>
+                    Page{' '}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{' '}
+                </span>
+                <span>
+                    | Go to page:{' '}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={e => {
+                            const page = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(page)
+                        }}
+                        style={{ width: '100px' }}
+                    />
+                </span>{' '}
+                <select
+                    value={pageSize}
+                    onChange={e => {
+                        setPageSize(Number(e.target.value))
+                    }}
+                >
+                    {[10, 15, 30].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
         </>
 
     )
