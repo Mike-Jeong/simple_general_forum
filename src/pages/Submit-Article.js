6b7e371f2e51d856d9041715abdfb6c2245d1358
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-19 20:03:57 +0900

    게시글 등록 페이지 수정

--- src/pages/Submit-Article.js
+++ src/pages/Submit-Article.js
@@ -1,14 +1,17 @@
 import React from "react";
- 
-const SubmitArticle = () =>  {
-     return (
-      <div>
-        <h2>게시글 등록 하는 곳</h2>
-            <p>게시글을 등록하는 폼이 생성되겠죠?</p>
-            <p>익명 아이디를 자동 생성해야할까요 아님</p>
-            <p>직접 생성하게 해야할까요</p>
+import './SubmitForm.css'
+
+const SubmitArticle = () => {
+    return (
+        <div>
+            <h2>게시글 등록</h2>
+            <div className='form-wrapper'>
+                <input className="title-input" type='text' placeholder='제목' />
+                <textarea className="text-area" placeholder='내용'></textarea>
+            </div>
+            <button className="submit-button">입력</button>
         </div>
     );
 }
- 
+
 export default SubmitArticle;
