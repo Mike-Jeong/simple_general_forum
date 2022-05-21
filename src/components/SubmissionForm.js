623bae7019746b0a2095ef83b88529103d6e28ca
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-21 15:43:54 +0900

    게시글 등록 페이지 비밀번호 입력란 수정 01

--- src/components/SubmissionForm.js
+++ src/components/SubmissionForm.js
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
+function checkNumber(e) {
+
+    e.preventDefault();
+    if (e.key === '.'
+        || e.key === '-'
+        || e.key >= 0 && e.key <= 9) {
+        return true;
+    }
+
+    return false;
+}
+
 const SubmissionForm = () => {
     const { register, handleSubmit } = useForm();
     const [result, setResult] = useState("");
@@ -13,7 +25,7 @@ const SubmissionForm = () => {
             <input className='title-input' {...register("title")} placeholder="재목" />
             <p><textarea className='text-area' {...register("article")} placeholder="내용" /></p>
             <input className='author-input' {...register("authors")} placeholder="작성자" />
-            <input maxlength="4" {...register("password")} placeholder="비밀번호" />
+            <input onkeypress= {(e) => checkNumber(e)} pattern=".{4,4}" required title="4자리 비밀번호를 입력해 주세요" {...register("password")} placeholder="비밀번호" />
             <p>{result}</p>
             <input type="submit" className='submit-button' />
         </form>
