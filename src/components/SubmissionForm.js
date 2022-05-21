66033f18e57df6168e2a91b167ec380c7e1703b4
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-05-21 18:42:54 +0900

    게시글 등록 페이지 비밀번호 입력란 수정 02

--- src/components/SubmissionForm.js
+++ src/components/SubmissionForm.js
@@ -1,34 +1,42 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
-function checkNumber(e) {
-
-    e.preventDefault();
-    if (e.key === '.'
-        || e.key === '-'
-        || e.key >= 0 && e.key <= 9) {
-        return true;
-    }
-
-    return false;
-}
-
 const SubmissionForm = () => {
     const { register, handleSubmit } = useForm();
+
     const [result, setResult] = useState("");
+    const [inputValue, setInputValue] = useState("");
+
     const onSubmit = (data) => setResult(JSON.stringify(data));
+
+
+    const checkNum = (e) => {
+
+        const value = e.target.value
+        console.log(value);
+        const newv = value.replace(/[^0-9]/g, '');
+        console.log(newv);
+        if (value != newv) {
+            inputValue = newv;
+        }
+        ck();
+    };
+
+    const ck = () => {
+        console.log("확인" + inputValue);
+    };
+
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-
-
             <input className='title-input' {...register("title")} placeholder="재목" />
             <p><textarea className='text-area' {...register("article")} placeholder="내용" /></p>
             <input className='author-input' {...register("authors")} placeholder="작성자" />
-            <input onkeypress= {(e) => checkNumber(e)} pattern=".{4,4}" required title="4자리 비밀번호를 입력해 주세요" {...register("password")} placeholder="비밀번호" />
+            <input onKeyUp={checkNum} value={inputValue} pattern=".{4,4}" required title="4자리 비밀번호를 입력해 주세요" {...register("password")} placeholder="비밀번호" />
             <p>{result}</p>
             <input type="submit" className='submit-button' />
         </form>
     );
 }
 export default SubmissionForm;
+
