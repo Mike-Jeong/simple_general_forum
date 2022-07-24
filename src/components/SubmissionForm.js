df31163d6a14c9fe591c6a0c3be909e356dc880c
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-24 13:18:23 +0900

    24072022 - Minor update in SubmissionForm.js

--- src/components/SubmissionForm.js
+++ src/components/SubmissionForm.js
@@ -1,41 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 const SubmissionForm = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, erros } = useForm();
 
     const [result, setResult] = useState("");
     const [inputValue, setInputValue] = useState("");
 
     const onSubmit = (data) => setResult(JSON.stringify(data));
 
+    const onError = (error) => { alert('4자리 비밀번호를 입력해 주세요.') };
 
     const checkNum = (e) => {
-
         const value = e.target.value
         console.log(value);
         const newv = value.replace(/[^0-9]/g, '');
         console.log(newv);
-        if (value != newv) {
-            inputValue = newv;
-        }
-        ck();
-    };
 
-    const ck = () => {
-        console.log("확인" + inputValue);
+        if (newv.length > 4) {
+            const newvover = newv.substring(0, 4);
+            console.log("sss" + newvover);
+            setInputValue(newvover);
+        } else {
+            setInputValue(newv);
+        }
     };
 
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit, onError)}>
             <input className='title-input' {...register("title")} placeholder="재목" />
             <p><textarea className='text-area' {...register("article")} placeholder="내용" /></p>
             <input className='author-input' {...register("authors")} placeholder="작성자" />
-            <input onKeyUp={checkNum} value={inputValue} pattern=".{4,4}" required title="4자리 비밀번호를 입력해 주세요" {...register("password")} placeholder="비밀번호" />
-            <p>{result}</p>
-            <input type="submit" className='submit-button' />
+            <input value={inputValue} {...register("password", { minLength: 4, onChange: (e) => { checkNum(e) } })} placeholder="비밀번호" />
+            <input type="submit" className='submit-button' value={"제출"} />
         </form>
+
     );
 }
 export default SubmissionForm;
